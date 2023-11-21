0ebcb338a269bb02b94a26bb972dca44fc065f8b
Your Name <you@example.com>
2023-11-21 15:15:55 +0000

    test

--- init.lua
+++ init.lua
@@ -7,26 +7,26 @@ require('go').setup {
 }
 
 
---local format_sync_grp = vim.api.nvim_create_augroup("GoImport", {})
---vim.api.nvim_create_autocmd("BufWritePre", {
---pattern = "*.go",
---callback = function()
---require('go.format').goimport()
---end,
---group = format_sync_grp,
---})
+local format_sync_grp = vim.api.nvim_create_augroup("GoImport", {})
+vim.api.nvim_create_autocmd("BufWritePre", {
+	pattern = "*.go",
+	callback = function()
+		require('go.format').goimport()
+	end,
+	group = format_sync_grp,
+})
 
---require("go.format").gofmt()  -- gofmt only
---require("go.format").goimport()  -- goimport + gofmt
+require("go.format").gofmt()    -- gofmt only
+require("go.format").goimport() -- goimport + gofmt
 
----- Run gofmt on save
+-- Run gofmt on save
 
---vim.api.nvim_create_autocmd("BufWritePre", {
---pattern = "*.go",
---callback = function()
---require('go.format').gofmt()
---end,
---group = format_sync_grp,
---})
+vim.api.nvim_create_autocmd("BufWritePre", {
+	pattern = "*.go",
+	callback = function()
+		require('go.format').gofmt()
+	end,
+	group = format_sync_grp,
+})
 
----- Run gofmt + goimport on save
+-- Run gofmt + goimport on save
